28fcd51e8dde2acb977c94180fbe781f2bad05e2
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-07 11:22:49 +0100

    Creata funzione che consente, cercando un artista, di ottenere gli album che ha realizzato

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+const searchArtist = 'https://www.theaudiodb.com/api/v1/json/1/searchalbum.php';
+
 var app = new Vue({
     el : '#root',
 
     data : {
-        searched : ''
+        searched : '',
+        albumsFound : []
     },
 
     methods : {
-
+        startSearch() {
+            if (this.searched != '') {
+                let searched = this.searched;
+                this.searched = '';
+                axios
+                    .get(searchArtist, {
+                        params : {
+                            s : searched
+                        }
+                    })
+                    .then((response) => {
+                        this.albumsFound = response.data.album;
+                    });
+            }
+        }
     }
 });
